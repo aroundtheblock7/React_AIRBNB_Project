33d71269c2d20a1268b942c4c71dfa5935fc2a30
Joseph DeAngeloi <josephdeangeloi@Josephs-MBP.home>
2023-04-27 10:53:18 -0400

    update

---
+++ App.js
@@ -0,0 +1,25 @@
+import React from "react"
+//import Joke from "./components/Joke"
+//import jokesData from "./jokesData"
+import Navbar from "./components/Navbar"
+import Hero from "./components/Hero"
+import Card from "./components/Card"
+import data from "./data"
+
+export default function App() {
+const cards = data.map(item => {
+    return (
+        <Card 
+            key={item.id}
+            {...item}
+        />  
+    )
+    }) 
+    return (
+        <div>
+            <Hero />
+            <Navbar />
+            {cards}
+        </div>
+    )
+}
